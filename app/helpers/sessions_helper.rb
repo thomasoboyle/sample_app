caee8d9d2992500c0f3e3bdde23b6e6de65a2bb9
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-01-31 17:42:49 -0800

    Friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -48,4 +48,15 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # Redirects to stored locaiton (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stories the URL trying to be accesed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
