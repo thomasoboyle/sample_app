a63ab62bf6a0bf791f2706f4457d85a4328e0cde
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-02-21 19:38:29 -0800

    Finishes reset password functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,6 +62,10 @@ class User < ActiveRecord::Base
     UserMailer.password_reset(self).deliver_now
   end
 
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
   # Convert email to all lower-case.
