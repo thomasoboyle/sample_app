0a91cad82d078513e2b5e3058b1c2b52e76e7c22
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-03-08 21:18:11 -0800

    Bug fix

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   
   def home
-  	@mircopost = current_user.microposts.build if logged_in?
+  	@micropost = current_user.microposts.build if logged_in?
   end
 
   def help
