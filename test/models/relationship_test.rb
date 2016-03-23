dcbfb52b53bfa15df1f3869138f7a8f628072a3a
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-03-22 22:25:56 -0700

    Starts relationship tests

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+  	@relationship = Relationship.new(follower_id: 1, followed_id: 2)
+  end
+  
+  
 end
