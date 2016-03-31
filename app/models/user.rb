b7e8bfe43fe2d401960dd6d45021ec440be8a3ed
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-03-30 17:17:09 -0700

    Bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
                                         foreign_key:  "follower_id",
                                         dependent:    :destroy
   has_many :passive_relationships,      class_name:   "Relationship",
-                                        foreign_key:  "following_id",
+                                        foreign_key:  "followed_id",
                                         dependent:    :destroy
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
