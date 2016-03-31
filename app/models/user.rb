b0d123661ec67379b2a319a8a2a45d2228ce4932
Thomas O'Boyle <thomashoboyle@gmail.com>
2016-03-30 17:41:30 -0700

    Adds routes for user relationships and follow/unfollow form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token, :activation_token, :rest_token
+  
   has_many :microposts, dependent:  :destroy
   has_many :active_relationships,       class_name:   "Relationship", 
                                         foreign_key:  "follower_id",
@@ -8,7 +10,7 @@ class User < ActiveRecord::Base
                                         dependent:    :destroy
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
-  attr_accessor :remember_token, :activation_token, :rest_token
+
   before_save   :downcase_email
   before_create :create_activation_digest
   before_save { self.email = email.downcase }
